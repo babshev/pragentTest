efbb74dd9af48268ef6017d997f0da0c10844a2f
Vandana Babshetti <vandana.babshetti@autodesk.com>
2024-03-06 23:06:04 +0530

    test exception catch

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -10,6 +10,11 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
+            int i = 0;
+            int j = 10;
+            int k = j / i;
+            Console.Write(k);
+            ;
         }
     }
 }
